850ceeab7e2eaa09b9ba8f1bd33d1827616dd0d7
David Barbero <barberod@outlook.com>
2023-06-27 17:24:43 -0400

    commit of wokring theme and build process

---
+++ js/scroll.js
@@ -0,0 +1,37 @@
+function scrollToTop() {
+    window.scroll({top: 0, left: 0, behavior: 'smooth'});
+}
+
+function debounce(func, wait = 10, immediate = true) {
+    let timeout;
+    return function() {
+        let context = this, args = arguments;
+        let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+let scrollPos = 0;
+const nav = document.querySelector('.scroll-top');
+
+function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY < scrollPos && window.pageYOffset > 400) {
+        // Scrolling UP
+        nav.classList.add('is-visible');
+        nav.classList.remove('is-hidden');
+    } else {
+        // Scrolling DOWN
+        nav.classList.add('is-hidden');
+        nav.classList.remove('is-visible');
+    }
+    scrollPos = windowY;
+}
+
+window.addEventListener('scroll', debounce(checkPosition));
